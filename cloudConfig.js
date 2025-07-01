7fc08bfc75fa07f89b2e20decb0ca026adfaae0b
Aaditya <aadityakuamr518@gmail.com>
2025-07-01 16:02:41 +0530

    inserted data

--- cloudConfig.js
+++ cloudConfig.js
@@ -4,7 +4,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 cloudinary.config({
     cloud_name:process.env.cloud_Name,
     api_key:process.env.cloud_API_Key,
-    api_secret:process.env.cloud_Secret_Nmae,
+    api_secret:process.env.cloud_Secret_Name,
 })
 
 const storage = new CloudinaryStorage({
